0bf0864f41e841b42760c9be42cc14a1af800f0e
CandyChoChin <toetetaunglin.88@gmail.com>
2026-02-21 21:15:59 +0630

    add reversed click interaction

--- script.js
+++ script.js
@@ -5,53 +5,77 @@ const modalTitle = document.getElementById("modalTitle");
 const modalMeaning = document.getElementById("modalMeaning");
 const closeModal = document.getElementById("closeModal");
 
-if (modal && closeModal) {
-  cards.forEach((card) => {
-  card.addEventListener("click", () => {
-    const img = card.querySelector("img").src;
-    const title = card.dataset.title;
-    const meaning = card.dataset.meaning;
+let currentUpright = "";
+let currentReverse = "";
 
-    modalImage.src = img;
-    modalTitle.textContent = title;
-    modalMeaning.textContent = meaning;
+if (modal && closeModal && modalImage) {
+  modalImage.addEventListener("click", () => {
+    modalImage.classList.toggle("reverse");
 
-    modal.classList.add("active");
+    if (modalImage.classList.contains("reverse")) {
+      modalMeaning.textContent = currentReverse;
+    } else {
+      modalMeaning.textContent = currentUpright;
+    }
   });
-});
 
-closeModal.addEventListener("click", () => {
-  modal.classList.remove("active");
-});
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      const img = card.querySelector("img").src;
+      const title = card.dataset.title;
+      currentUpright = card.dataset.meaning;
+      currentReverse = card.dataset.reversed;
 
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) {
+      modalImage.src = img;
+      modalImage.classList.remove("reverse");
+      modalTitle.textContent = title;
+      modalMeaning.textContent = currentUpright;
+
+      modal.classList.add("active");
+    });
+  });
+
+  // ===== close the pop up =====
+
+  const handleClose = () => {
     modal.classList.remove("active");
-  }
-});
+    modalImage.classList.remove("reverse");
+  };
+
+  closeModal.addEventListener("click", handleClose);
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      handleClose();
+    }
+  });
 }
 
 // ==== upper is library page ====
-
 
 const menuIcon = document.getElementById("menuIcon");
 const sideContainer = document.querySelector(".side-container");
 // sideContainer is background when click the icon
 
-if(menuIcon && sideContainer){
-  
+if (menuIcon && sideContainer) {
+  menuIcon.addEventListener("click", () => {
+    sideContainer.classList.toggle("active");
+  });
 
-menuIcon.addEventListener("click", () => {
-
-
-
-  sideContainer.classList.toggle('active');
-
-});
-
-sideContainer.addEventListener('click', (e) => {
-  if(e.target === sideContainer){
-    sideContainer.classList.remove('active');
-  }
-})
+  sideContainer.addEventListener("click", (e) => {
+    if (e.target === sideContainer) {
+      sideContainer.classList.remove("active");
+    }
+  });
 }
+
+
+// ==== upper is menu toggle ====
+
+
+
+
+
+
+
+
