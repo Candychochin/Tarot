04cb346419f3eadc2c6367183f220f5b5f0b7e86
CandyChoChin <toetetaunglin.88@gmail.com>
2026-02-22 21:52:09 +0630

    add search feature

--- script.js
+++ script.js
@@ -69,13 +69,51 @@ if (menuIcon && sideContainer) {
   });
 }
 
-
 // ==== upper is menu toggle ====
 
+const searchInput = document.getElementById("searchInput");
+const containers = document.querySelectorAll(
+  `.arcana-card-container, 
+      .cup-card-container, 
+      .pentacles-card-container, 
+      .sword-card-container, 
+      .wand-card-container`,
+);
+const noResult = document.getElementById("noResult");
 
+if (searchInput) {
+  searchInput.addEventListener("input", () => {
+    const searchValue = searchInput.value.toLowerCase();
 
+    cards.forEach((card) => {
+      const title = card.dataset.title.toLowerCase();
+      const meaning = card.dataset.meaning.toLowerCase();
 
+      if (title.includes(searchValue) || meaning.includes(searchValue)) {
+        card.style.display = "";
+      } else {
+        card.style.display = "none";
+      }
+    });
+    containers.forEach((container) => {
+      const visibleCards = Array.from(
+        container.querySelectorAll(".card-loop-cards"),
+      ).filter((card) => card.style.display !== "none");
+      if (visibleCards.length === 0) {
+        container.style.display = "none";
+      } else {
+        container.style.display = "";
+      }
+    });
 
+    const anyVisibleCard = Array.from(cards).some(
+      (card) => card.style.display !== "none",
+    );
 
-
-
+    if (!anyVisibleCard) {
+      noResult.style.display = "block";
+    } else {
+      noResult.style.display = "none";
+    }
+  });
+}
